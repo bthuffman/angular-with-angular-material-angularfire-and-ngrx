6eb377fa7f3a693e574a1a891776671f75eb2b85
Brandon Huffman <bt_huffman@msn.com>
2019-06-17 13:51:28 -0600

    Trying again

--- angular-angularfire/src/app/app.module.ts
+++ angular-angularfire/src/app/app.module.ts
@@ -5,18 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductComponent } from './product/product.component';
+import { ProductsService } from './products.service';
+import { HomeComponent } from './home/home.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    ProductComponent
+    ProductComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
