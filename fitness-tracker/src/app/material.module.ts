d460f340d4aedbf1fdbdffe0390ec19553a65851
Brandon Huffman <bt_huffman@msn.com>
2019-06-21 11:25:52 -0600

    Added some Angular material buttons to the app.component.html. Will be replaced next commit.

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -1,10 +1,11 @@
 // Importing all Angular Material components want to use into this file. 
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule, MatIcon } from '@angular/material/icon'
 
 @NgModule({
-    imports: [MatButtonModule],
-    exports: [MatButtonModule]
+    imports: [MatButtonModule, MatIconModule],
+    exports: [MatButtonModule, MatIconModule]
 })
 
 export class MaterialModule {} 
