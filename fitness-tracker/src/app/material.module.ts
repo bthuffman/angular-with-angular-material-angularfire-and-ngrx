2ca951e068f8462fc17cecd2e12c0010e3a0fce9
Brandon Huffman <bt_huffman@msn.com>
2019-06-25 14:20:15 -0600

    Added styles and text to the card. Added functionality such that current training only shows up if ongoingTraining is true otherwise the new excercise card shows up. Added functionality to start a training and created a progress circle that increments through time and stops once it hits 100% or when the user hits the stop button.

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -11,12 +11,14 @@ import { MatButtonModule,
     MatToolbarModule, 
     MatListModule, 
     MatTabsModule,
-    MatCardModule
+    MatCardModule,
+    MatSelectModule, 
+    MatProgressSpinnerModule
 } from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule,MatTabsModule, MatCardModule], 
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,MatToolbarModule, MatListModule, MatTabsModule, MatCardModule]
+    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule,MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule], 
+    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule]
 })
 
 export class MaterialModule {} 
