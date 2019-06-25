9e04ae803a2767e018b99c18056c6c4b91117217
Brandon Huffman <bt_huffman@msn.com>
2019-06-25 09:38:56 -0600

    Added various styles and functionality to the toolbar and sidenavitgation and sperated them from the app.component.html into their own seperate navigation/header and navigation/sidebar components respectively.

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -8,12 +8,13 @@ import { MatButtonModule,
     MatNativeDateModule,
     MatCheckboxModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule, 
+    MatListModule
 } from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule], 
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,MatToolbarModule]
+    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule], 
+    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,MatToolbarModule, MatListModule]
 })
 
 export class MaterialModule {} 
