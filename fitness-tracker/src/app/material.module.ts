370787351ea57562377e25b0d781077fc260fdd3
Brandon Huffman <bt_huffman@msn.com>
2019-06-25 13:41:01 -0600

    Added tabs to the training html and mat-card content to the new-training component and both of those modules to material.module.ts.

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -9,12 +9,14 @@ import { MatButtonModule,
     MatCheckboxModule,
     MatSidenavModule,
     MatToolbarModule, 
-    MatListModule
+    MatListModule, 
+    MatTabsModule,
+    MatCardModule
 } from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule], 
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,MatToolbarModule, MatListModule]
+    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule,MatTabsModule, MatCardModule], 
+    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,MatToolbarModule, MatListModule, MatTabsModule, MatCardModule]
 })
 
 export class MaterialModule {} 
